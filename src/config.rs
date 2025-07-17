583b8c31b28dc02e0a0971d74589aa174ef22f83
jwangb7111 <34438897+jwangb7111@users.noreply.github.com>
2025-07-18 00:01:54 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.jwang.online"];
-pub const RS_PUB_KEY: &str = "gEUVJDaFNxFVvv8YaAMDXhqmITf2WGSeFAkIOvigooA=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust"];
+pub const RS_PUB_KEY: &str = "=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
